bc851d4ac67e40d3ed0ce61fb79d7ea40f5dff9e
Lise <lise.p.poirier@gmail.com>
2019-04-15 19:15:47 +0200

    Error js corrigée

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,14 @@
-var deleteLog = false;
-  $(document).ready(function() {
-      $('#pagepiling').pagepiling({
-        menu: '#menu',
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6',],
-        sectionsColor: ['#344955', '#f78c28', '#344955', '#f78c28', '#344955', '#f78c28'],
-        loopTop: true,
-        loopBottom: true
-    });
-});
+var w = window.innerWidth;
+console.log(w);
+if (w > 960) {
+  var deleteLog = false;
+    $(document).ready(function() {
+        $('#pagepiling').pagepiling({
+          menu: '#menu',
+          anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6',],
+          sectionsColor: ['#344955', '#f78c28', '#344955', '#f78c28', '#344955', '#f78c28'],
+          loopTop: true,
+          loopBottom: true
+      });
+  });
+}
