c68a7e0005927f955919f6b517aab102a363ec4c
Lise <lise.p.poirier@gmail.com>
2019-04-16 19:38:28 +0200

    partie ksako

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,9 +6,37 @@ if (w > 1000) {
         $('#pagepiling').pagepiling({
           menu: '#menu',
           anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6',],
-          sectionsColor: ['#344955', '#f78c28', '#344955', '#f78c28', '#344955', '#f78c28'],
+          sectionsColor: ['#344955', '#344955', '#344955', '#f78c28', '#344955', '#f78c28'],
           loopTop: true,
           loopBottom: true
       });
   });
 }
+
+
+//SLIDER
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 3,
+    //autoplay: {
+      //delay: 1000,
+      //disableOnInteraction: false,
+    //},
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    breakpoints: {
+      1000: {
+        slidesPerView: 1,
+      },
+    }
+});
