230bb156be4419046e87af796978c489fe854752
Lise <lise.p.poirier@gmail.com>
2019-04-20 18:39:00 +0200

    trololo

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,42 +1,67 @@
 var w = window.innerWidth;
 console.log(w);
 if (w > 1000) {
-  var deleteLog = false;
-    $(document).ready(function() {
+    var deleteLog = false;
+    $(document).ready(function () {
         $('#pagepiling').pagepiling({
-          menu: '#menu',
-          anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6',],
-          sectionsColor: ['#344955', '#344955', '#f78c28', '#f78c28', '#344955', '#f78c28'],
-          loopTop: true,
-          loopBottom: true
-      });
-  });
+            menu: '#menu',
+            anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', ],
+            sectionsColor: ['#2d4753', '#2d4753', '#f78c28', '#2d4753', '#f78c28', '#2d4753'],
+            loopTop: true,
+            loopBottom: true
+        });
+    });
 }
 
 
-//SLIDER
-var swiper = new Swiper('.swiper-container', {
+//SLIDER PHONE
+var swiper = new Swiper('#swiper-present', {
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 3,
     autoplay: {
-      delay: 1000,
-      disableOnInteraction: false,
+        delay: 3000,
+        disableOnInteraction: false,
     },
     coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows : true,
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
     },
-    pagination: {
-      el: '.swiper-pagination',
+    navigation: {
+        nextEl: '.swiper-button-next-present',
+        prevEl: '.swiper-button-prev-present',
     },
     breakpoints: {
-      1000: {
-        slidesPerView: 1,
-      },
+        1000: {
+            slidesPerView: 1,
+        },
+    }
+});
+
+//SLIDER ACTU
+var swiper = new Swiper('#swiper-actu', {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next-actu',
+        prevEl: '.swiper-button-prev-actu',
+    },
+    //autoplay: {
+    //delay: 1000,
+    //disableOnInteraction: false,
+    //},
+    breakpoints: {
+        1250: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
     }
 });
